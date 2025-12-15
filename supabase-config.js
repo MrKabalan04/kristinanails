4c3795505d3d42cf1907056a334da6fab737edc6
MrKabalan04 <159710101+MrKabalan04@users.noreply.github.com>
2025-12-15 23:08:31 +0200

    test

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,12 @@
-const supabaseUrl = 'https://gxcdrkmveesttkyejzza.supabase.co';
+// supabase-config.js
+
+const supabaseUrl = "https://gxcdrkmveesttkyejzza.supabase.co";
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd4Y2Rya212ZWVzdHRreWVqenphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgzNjAyMTgsImV4cCI6MjA3MzkzNjIxOH0.TtcwVbzaZmRVopUIQhnMvUc-l_s0muUiMxb_iXbSZuE';
 
-import { createClient as supabaseCreateClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+export function createClient() {
+  if (!window.supabase) {
+    throw new Error("Supabase SDK not loaded. Check script order in index.html");
+  }
 
-export const createClient = () => supabaseCreateClient(supabaseUrl, supabaseKey);
+  return window.supabase.createClient(supabaseUrl, supabaseAnonKey);
+}
